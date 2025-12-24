4437f5084255fc45c7ed0d2b825210e87d285d70
Xariskaramf <>
2025-12-24 15:26:05 +0200

    Refactored Folder structure of Footer and Header components with associated styles

--- frontend/src/components/Layout/Header/Header.jsx
+++ frontend/src/components/Layout/Header/Header.jsx
@@ -1,39 +1,42 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { useAccess } from '../../context/AccessContext';
+import { useAccess } from '@/context/AccessContext';
+import logo from '@/assets/logo.png';
 import styles from './Header.module.css';
-import logo from '../../assets/logo.png';
-
 
 const Header = () => {
   const { hasAccess, logout } = useAccess();
   const navigate = useNavigate();
-  const handleLogout = async() => {
+  const handleLogout = async () => {
     await logout();
-    navigate ('./auth');
-  }
+    navigate("./auth");
+  };
 
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.logo}>
-        <img 
-            src={logo} 
-            alt="Electronics R&R" 
-            className={styles.logoImage}
-            />
-            <span>
-               Electronics <strong>R&amp;R</strong>
-            </span>
+          <img src={logo} alt="Electronics R&R" className={styles.logoImage} />
+          <span>
+            Electronics <strong>R&amp;R</strong>
+          </span>
         </div>
-        
+
         <nav className={styles.nav}>
-          <Link to="/dashboard" className={styles.link}>Home</Link>
-          
+          <Link to="/dashboard" className={styles.link}>
+            Home
+          </Link>
+
           {hasAccess ? (
             <>
-              <Link to="/dashboard" className={styles.link}>☷ Dashboard</Link>
-              <Link to="/requests" className={styles.link}>🎟 Requests</Link>
-              <Link to="/create-ticket" className={styles.link}>+ New Request</Link>
+              <Link to="/dashboard" className={styles.link}>
+                ☷ Dashboard
+              </Link>
+              <Link to="/requests" className={styles.link}>
+                🎟 Requests
+              </Link>
+              <Link to="/create-ticket" className={styles.link}>
+                + New Request
+              </Link>
               <button onClick={handleLogout} className={styles.authButton}>
                 Sign Out
               </button>
