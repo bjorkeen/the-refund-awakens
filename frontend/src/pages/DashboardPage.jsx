d824e0fa103d6186fa20cb1c31a851b6da4620a6
Xariskaramf <>
2025-12-24 15:27:26 +0200

    Renamed DashboardPage component and added role-based rendering for user dashboards

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,25 @@
+import { useAccess } from "@/context/AccessContext.jsx";
+import CustomerDashboard from "@/components/Dashboard/CustomerDashboard";
+import AdminDashboard from "@/components/Dashboard/AdminDashboard";
+import TechnicianDashboard from "@/components/Dashboard/TechnicianDashboard"
+
+const DashboardPage = () => {
+  const { user } = useAccess();
+
+  if (!user)
+    return <div className="p-8 text-center">Loading user profile...</div>;
+
+  // Smart Switching Logic
+  switch (user.role) {
+    case 'Technician':
+      return <TechnicianDashboard />;
+    case 'Admin':
+    case 'Manager':
+      return <AdminDashboard />;
+    case 'Customer':
+    default:
+      return <CustomerDashboard />;
+  }
+};
+
+export default DashboardPage;
