330e71b55fd48521a16c46f7188cf05bf1739a02
despoina-makridou <despoinamakridou2@gmail.com>
2025-12-29 16:29:24 +0200

    Update api.jsx
    change api

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+//fallback : use environment variable or default to relative path to let nginx handle proxying
+const API_URL = import.meta.env.VITE_API_URL || '/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:5050/api',
+  baseURL: API_URL, 
   withCredentials: true,
 });
 
